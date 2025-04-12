d23413df813e00c890d07908f1c0112492c3df08
Junyu Li <70296613+Junyu06@users.noreply.github.com>
2025-04-12 09:44:44 -0400

    fix the https issuel

--- Backend/app.py
+++ Backend/app.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 app.register_blueprint(api_bp, url_prefix="/api")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(app.run(ssl_context=('cert.pem', 'key.pem'), debug=True), debug=True)
